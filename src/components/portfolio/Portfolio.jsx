5254fe1480d6d855154dfd4ad37e607400a9421f
paulinaweb <124458081+paulinaweb@users.noreply.github.com>
2024-02-03 19:03:52 +0100

    updated number

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -4,7 +4,7 @@ import "./portfolio.scss";
 import { motion, useScroll, useSpring, useTransform } from "framer-motion";
 import img1 from "../../assets/img1.png";
 import img2 from "../../assets/img2.png";
-import img3 from "../../assets/img3.png";
+import img4 from "../../assets/img4.png";
 
 const items = [
   {
